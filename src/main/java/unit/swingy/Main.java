ad2f6801b0d4e901ee22d9027a0c18e819bfb65f
Litavets <litavets@gmail.com>
2019-10-11 14:49:35 +0300

    Starting

--- src/main/java/unit/swingy/Main.java
+++ src/main/java/unit/swingy/Main.java
@@ -1,9 +1,33 @@
 package unit.swingy;
 
 public class Main {
+
+	static boolean guiMode;
+
+	public static void printUsage(){
+		System.out.println("Choose the game mode by providing an argument.");
+		System.out.println("\t$java -jar swingy.jar console\nor\n\t$java -jar swingy.jar gui");
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println("\n>> An awesome RPG will be here! Soon.... <<\n");
+		if (args.length == 1) {
+			switch (args[0]) {
+				case "gui":
+					guiMode = true;
+					break;
+				case "console":
+					guiMode = false;
+					break;
+				default:
+					printUsage();
+			}
+		} else {
+			printUsage();
+		}
 
+		System.out.println(guiMode);
 	}
+
+
 }
